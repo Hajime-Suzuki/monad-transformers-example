29e9065a1fb8b892b8d2ada1eab29da5d81fe7f8
Hajime Suzuki <hajime.doublebass@gmail.com>
2020-05-30 10:48:20 +0200

    update getConfig

--- src/Config.hs
+++ src/Config.hs
@@ -1,20 +1,35 @@
 module Config where
 
-import           Control.Monad.Trans.Maybe
+import           Control.Monad                  ( mzero )
+import           Control.Monad.Except
 import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Maybe
+import           Data.Maybe                     ( isNothing )
 import           LoadEnv                        ( loadEnvFrom )
 import           System.Environment             ( lookupEnv )
 
-data Config = Config {
-  dbString :: String
-}
 
+data Config
+  = Config
+      { dbString :: String
+      , port     :: String
+      , host     :: String
+      }
+  deriving (Show)
 
-getConfig :: MaybeT IO String
+data ConfigError = ConfigError deriving (Show)
+
+getConfig :: ExceptT ConfigError IO Config
 getConfig = do
-  liftIO . loadEnvFrom $ ".env"
-  connectionString <- liftIO . lookupEnv $ "PG_CONNECTION_STRING"
-  case connectionString of
-    Just v  -> return v
-    Nothing -> fail "no connection string found"
+  dbString <- liftIO $ lookupEnv "DB_CONNECTION_STRING"
+  port     <- liftIO $ lookupEnv "PORT"
+  host     <- liftIO $ lookupEnv "HOST"
+
+  let mayConfig = Config <$> dbString <*> port <*> host
+
+  case mayConfig of
+    Nothing     -> throwError ConfigError
+    Just config -> return $ config
+
+
 
